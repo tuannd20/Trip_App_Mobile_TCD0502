4d979f073de1d37825c5c07eab925a4d304e9dc1
SMD283-TuanNguyen <tuan.nguyenduy@smartdev.com>
2022-09-23 17:55:49 +0700

    init porject trip app

---
+++ app/src/main/java/com/androidtopup/tripapptcd0502/MainActivity.java
@@ -0,0 +1,43 @@
+package com.androidtopup.tripapptcd0502;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.androidtopup.tripapptcd0502.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        ActivityMainBinding binding;
+
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeTripFragment());
+
+        binding.bottomNavigationView.setOnItemReselectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeTripFragment());
+                    break;
+                case R.id.upload:
+                    replaceFragment(new UploadCloudFragment());
+                    break;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayoutApp, fragment);
+        fragmentTransaction.commit();
+    }
+}
