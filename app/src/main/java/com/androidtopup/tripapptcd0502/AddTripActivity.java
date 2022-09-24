99bb9b1269852dcad4967b166720eea960f84842
SMD283-TuanNguyen <tuan.nguyenduy@smartdev.com>
2022-09-24 09:15:54 +0700

    refactor code

--- app/src/main/java/com/androidtopup/tripapptcd0502/AddTripActivity.java
+++ app/src/main/java/com/androidtopup/tripapptcd0502/AddTripActivity.java
@@ -1,19 +1,15 @@
 package com.androidtopup.tripapptcd0502;
 
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
-import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
@@ -30,7 +26,6 @@ public class AddTripActivity extends AppCompatActivity {
     RadioButton rb_no;
     TextInputLayout description_input;
     Button add_trip;
-    String valueAssessment;
     Button cancel_btn;
 
     @Override
@@ -39,42 +34,9 @@ public class AddTripActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_trip);
 
         handleDateTrip();
-
-        name_input = (TextInputLayout)findViewById(R.id.inputNameOfTrip);
-        destination_input = (TextInputLayout)findViewById(R.id.inputDestination);
-        date_of_trip_input = (TextInputLayout)findViewById(R.id.dateOfTripInput);
-        description_input = (TextInputLayout)findViewById(R.id.inputDescription);
-
-        cancel_btn = findViewById(R.id.button_cancel);
-        cancel_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-
-        add_trip = findViewById(R.id.button_add);
-        add_trip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                 String strName = name_input.getEditText().getText().toString();
-                 String strDestination = destination_input.getEditText().getText().toString();
-                 String strDate = date_of_trip_input.getEditText().getText().toString();
-                 String strDescription = description_input.getEditText().getText().toString();
-
-                if (rb_yes.isChecked()){
-                    valueAssessment = rb_yes.getText().toString();
-                } else if (rb_no.isChecked()) {
-                    valueAssessment = rb_no.getText().toString();
-                }
-                Log.d("Data", valueAssessment + strDate + strName + strDestination + strDescription);
-//                ExpenseAppDataBaseHelper db = new ExpenseAppDataBaseHelper(AddTripActivity.this);
-//                db.create(strName, strDestination, strDate, valueAssessment, strDescription);
-            }
-        });
+        handleCancelBtn();
+        getInputData();
     }
-
-
 
     private void handleDateTrip() {
         TextInputEditText mTextInputEditText = findViewById(R.id.date_trip);
@@ -92,12 +54,62 @@ public class AddTripActivity extends AppCompatActivity {
         });
     }
 
+    private void getInputData() {
+        name_input = (TextInputLayout)findViewById(R.id.inputNameOfTrip);
+        destination_input = (TextInputLayout)findViewById(R.id.inputDestination);
+        date_of_trip_input = (TextInputLayout)findViewById(R.id.dateOfTripInput);
+        description_input = (TextInputLayout)findViewById(R.id.inputDescription);
+        add_trip = findViewById(R.id.button_add);
+        add_trip.setOnClickListener(new View.OnClickListener() {
+            String strName = name_input.getEditText().getText().toString().trim();
+            String strDestination = destination_input.getEditText().getText().toString().trim();
+            String strDate = date_of_trip_input.getEditText().getText().toString().trim();
+            String strDescription = description_input.getEditText().getText().toString().trim();
+            String value = getValueAssessment();
+            @Override
+            public void onClick(View view) {
+                insertDataOfTrip(strName, strDestination, strDate, value, strDescription);
+            }
+        });
+    }
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void updateDOB(LocalDate dot){
         TextInputEditText dobText = (TextInputEditText) findViewById(R.id.date_trip);
         DateTimeFormatter formatDateOfTrip = DateTimeFormatter.ofPattern( "dd/MM/uuuu" ) ;
         String result = dot.format( formatDateOfTrip ) ;
         dobText.setText(result);
+    }
+
+    private void handleCancelBtn() {
+        cancel_btn = findViewById(R.id.button_cancel);
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private String getValueAssessment() {
+        String valueAssessment = new String();
+        rb_yes = findViewById(R.id.rb_yes);
+        rb_no = findViewById(R.id.rb_no);
+
+        if (rb_yes.isChecked()){
+            valueAssessment = rb_yes.getText().toString().trim();
+            return valueAssessment;
+        }
+        if (rb_no.isChecked()) {
+            valueAssessment = rb_no.getText().toString().trim();
+            return valueAssessment;
+        }
+        return valueAssessment;
+    }
+
+    private void insertDataOfTrip(String nameTrip, String destination, String date, String assessment, String desc) {
+        ExpenseAppDataBaseHelper db = new ExpenseAppDataBaseHelper(AddTripActivity.this);
+        db.create(nameTrip, destination, date, assessment, desc);
     }
 
 }
